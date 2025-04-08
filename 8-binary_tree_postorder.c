daf907bbc224fca9d2e02b98ac9b6250930648c5
Juan Pablo <jpbriceno2001@gmail.com>
2025-04-08 02:40:36 +0000

    Task 8,9,10,11,12,13

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,12 +1,19 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_postorder - Goes through a binary tree using post-order traversal
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node
+ *
+ * Description: Visits the left subtree, then the right subtree,
+ * then the current node
+ */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (!tree || !func)
-        return;
+	if (tree == NULL || func == NULL)
+		return;
 
-    binary_tree_postorder(tree->left, func);
-    binary_tree_postorder(tree->right, func);
-    func(tree->n);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
-
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,15 +1,20 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: Height of the tree, or 0 if tree is NULL
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left, right;
+	size_t left, right;
 
-    if (!tree || (!tree->left && !tree->right))
-        return (0);
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
 
-    left = binary_tree_height(tree->left);
-    right = binary_tree_height(tree->right);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
-    return (1 + (left > right ? left : right));
+	return (1 + (left > right ? left : right));
 }
-
