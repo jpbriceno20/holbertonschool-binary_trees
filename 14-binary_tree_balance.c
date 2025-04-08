6a3bddc3b9e98bd25cab11c422b4d13dbd8480b4
Juan Pablo <jpbriceno2001@gmail.com>
2025-04-08 02:50:18 +0000

    Task 14,18

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,15 +1,42 @@
 #include "binary_trees.h"
 
-int binary_tree_balance(const binary_tree_t *tree)
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node
+ *
+ * Return: Height, or 0 if tree is NULL
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-    int left, right;
+	size_t left, right;
 
-    if (!tree)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left = (int)binary_tree_height(tree->left);
-    right = (int)binary_tree_height(tree->right);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
-    return (left - right);
+	if (tree->left || tree->right)
+		return (1 + (left > right ? left : right));
+	else
+		return (0);
 }
 
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: Pointer to the root node
+ *
+ * Return: Balance factor, or 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = (int)binary_tree_height(tree->left);
+	right = (int)binary_tree_height(tree->right);
+
+	return (left - right);
+}
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,10 +1,32 @@
 #include "binary_trees.h"
 
-binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+/**
+ * binary_tree_sibling - Finds the sibling of a node
+ * @node: Pointer to the node to find the sibling
+ *
+ * Return: Pointer to sibling, or NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node || !node->parent || !node->parent->parent)
-        return (NULL);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
 
-    return (binary_tree_sibling(node->parent));
+	if (node->parent->left == node)
+		return (node->parent->right);
+
+	return (node->parent->left);
 }
 
+/**
+ * binary_tree_uncle - Finds the uncle of a node
+ * @node: Pointer to the node to find the uncle
+ *
+ * Return: Pointer to the uncle node, or NULL if no uncle or node is NULL
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
+
+	return (binary_tree_sibling(node->parent));
+}
